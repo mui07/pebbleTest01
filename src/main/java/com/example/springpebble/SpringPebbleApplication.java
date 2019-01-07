af93e464185d2aadccb5e38dad8906d473defc18
mui <mui07@naver.com>
2019-01-07 18:16:33 +0900

    commit message

---
+++ src/main/java/com/example/springpebble/SpringPebbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpebble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringPebbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPebbleApplication.class, args);
+	}
+}
